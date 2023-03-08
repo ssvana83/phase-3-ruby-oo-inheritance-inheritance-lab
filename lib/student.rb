2953fc0e13caac64b9959d6e02b5c011a7685879
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-08 10:39:35 -0500

    DONE

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize (knowledge = [])
+  @knowledge = []    
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    includes = @knowledge
+  end
 
 end
